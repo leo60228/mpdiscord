f3cc2ce3e82d72e762895eb7db8466b0e575c751
leo60228 <leo@60228.dev>
2021-01-29 21:55:48 -0500

    mpd

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,13 @@ use crossbeam::queue::SegQueue;
 use discord_game_sdk::{Activity, CreateFlags, Discord, EventHandler, User};
 use futures::prelude::*;
 use futures::stream::FuturesUnordered;
+use serde::Deserialize;
+use std::fmt::Write;
 use std::pin::Pin;
 use std::sync::Arc;
-use std::time::Duration;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufStream};
+use tokio::net::TcpStream;
 use tokio::sync::{oneshot, watch};
 
 #[derive(Clone)]
@@ -240,9 +244,76 @@ async fn run(handle: EventHandlerHandle) -> Result<!> {
     let user = handle.user().await?;
     println!("logged in as {:#?}", user);
 
-    handle.update_activity(Activity::empty()).await?;
+    let mut stream = BufStream::new(TcpStream::connect("localhost:6600").await?);
 
-    future::pending().await
+    #[derive(Deserialize)]
+    struct Song {
+        title: Option<String>,
+        artist: Option<String>,
+        album: Option<String>,
+    }
+
+    loop {
+        let time = SystemTime::now();
+        println!("getting status");
+        stream.write_all(b"status\n").await?;
+        stream.flush().await?;
+        let mut status_resp = String::new();
+        while status_resp.trim().lines().last() != Some("OK") {
+            stream.read_line(&mut status_resp).await?;
+        }
+        let status: mparsed::Status = mparsed::parse_response(status_resp.lines())?;
+
+        println!("getting song");
+        stream.write_all(b"currentsong\n").await?;
+        stream.flush().await?;
+        let mut song_resp = String::new();
+        while song_resp.trim().lines().last() != Some("OK") {
+            stream.read_line(&mut song_resp).await?;
+        }
+        let song: Song = mparsed::parse_response(song_resp.lines())?;
+        println!("got song");
+
+        let mut activity = Activity::empty();
+
+        if let Some(title) = &song.title {
+            println!("{}", title);
+            activity.with_details(title);
+        }
+
+        let mut state = String::new();
+
+        if let Some(artist) = &song.artist {
+            write!(state, "by {} ", artist)?;
+        }
+
+        if let Some(album) = &song.album {
+            write!(state, "(album: {})", album)?;
+        }
+
+        println!("{}", state);
+
+        activity.with_state(&state);
+
+        if status.state == mparsed::State::Play {
+            if let Some(elapsed) = status.elapsed {
+                println!("Elapsed: {:?}", elapsed);
+
+                let start = time - elapsed;
+                let since_epoch = start.duration_since(UNIX_EPOCH)?;
+                activity.with_start_time(since_epoch.as_secs() as _);
+            }
+        }
+
+        handle.update_activity(activity).await?;
+
+        stream.write_all(b"idle\n").await?;
+        stream.flush().await?;
+        let mut idle_resp = String::new();
+        while idle_resp.trim().lines().last() != Some("OK") {
+            stream.read_line(&mut idle_resp).await?;
+        }
+    }
 }
 
 fn main() -> Result<!> {
