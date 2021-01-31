d6f1b7910a601199b763943f074836ea80b61c75
leo60228 <leo@60228.dev>
2021-01-31 15:20:33 -0500

    split discord thread starting into lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,14 @@ use discord::DiscordHandle;
 use log::*;
 use mpd::Mpd;
 use std::sync::Arc;
+use tokio::task;
 
 pub mod config;
 mod conversions;
 pub mod discord;
 pub mod mpd;
 
-pub async fn run(handle: DiscordHandle, config: Arc<Config>) -> Result<!> {
+pub async fn run_discord(handle: DiscordHandle, config: Arc<Config>) -> Result<!> {
     let user = handle.user().await?;
     info!("logged in as @{}#{}", user.username(), user.discriminator());
 
@@ -36,4 +37,24 @@ pub async fn run(handle: DiscordHandle, config: Arc<Config>) -> Result<!> {
 
         mpd.idle().await?;
     }
+}
+
+pub async fn run(config: Arc<Config>) -> Result<!> {
+    let discord_client_id = config.discord_client_id;
+
+    discord::run_discord_thread(
+        move |handle| {
+            info!("connected to discord");
+
+            let config = config.clone();
+
+            let fut = async move {
+                run_discord(handle, config).await.unwrap();
+            };
+            let fut_handle = task::spawn(fut);
+            move || fut_handle.abort()
+        },
+        discord_client_id,
+    )
+    .await
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 #![feature(never_type)]
 
 use anyhow::{Context, Result};
-use log::*;
-use mpdiscord::{config::read_config, discord::run_discord_thread, run};
+use mpdiscord::{config::read_config, run};
 use simple_logger::SimpleLogger;
 use std::env::args_os;
-use tokio::task;
 
 #[tokio::main]
 async fn main() -> Result<!> {
@@ -14,21 +12,5 @@ async fn main() -> Result<!> {
     let config_path = args_os().nth(1).context("Missing configuration path!")?;
     let config = read_config(&config_path).await?;
 
-    let discord_client_id = config.discord_client_id;
-
-    run_discord_thread(
-        move |handle| {
-            info!("connected to discord");
-
-            let config = config.clone();
-
-            let fut = async move {
-                run(handle, config).await.unwrap();
-            };
-            let fut_handle = task::spawn(fut);
-            move || fut_handle.abort()
-        },
-        discord_client_id,
-    )
-    .await
+    run(config).await
 }
